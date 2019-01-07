e493b69d30894daf5e6a1430bba450fdafd5f631
Steve Newsted <snewsted@redhat.com>
2019-01-07 12:02:59 -0500

    cleaning up original start script

--- start-all.sh
+++ start-all.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-for i in `cat hosts|grep -v \\\\[`;
-do 
-	virsh start $i
-	sleep 5
-done
+HOSTSFILE=hosts
+if [ ! -r "$HOSTSFILE" ]; then
+	echo "${HOSTSFILE} is missing."
+	exit 10
+fi
 
+# a safer, cleaner loop over the hosts file to read in lines, rather than words
+grep -E '^[^\[ ]' < $HOSTSFILE | while IFS= read -r i
+do 
+	virsh start "$i"
+	#sleep 5
+done
